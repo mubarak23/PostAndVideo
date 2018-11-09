8aa99f02494830f44dcdfba7b9fb920f1b626333
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-11-09 18:56:23 +0100

    create the post and video migration model and resource controller

---
+++ database/migrations/2018_11_09_174852_create_videos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->nullable();
+            $table->string('name');
+            $table->string('email');
+            $table->string('title');
+            $table->longText('video_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
