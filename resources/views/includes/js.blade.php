4d442c1d5c0385848d6afb041b27b26cc9969bfb
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-11-09 18:14:24 +0100

    create the initial landing page with navbar

---
+++ resources/views/includes/js.blade.php
@@ -0,0 +1,11 @@
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
---
+++ resources/views/includes/styles.blade.php
@@ -0,0 +1,9 @@
+<!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
